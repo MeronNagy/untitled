5ef22bd2576e1855ac21f9f9118f586d2957b1dc
MeronNagy <nagy.meron@gmail.com>
2024-11-05 20:07:27 +0100

    simple test to check ci

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", greet());
+}
+
+pub fn greet() -> &'static str {
+    "Hello, world!"
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_greet() {
+        assert_eq!(greet(), "Hello, world!");
+    }
 }
